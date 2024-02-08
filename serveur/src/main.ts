ded4cfaaef25a165c63ab09de6c26476dcecc621
Sikkael <Mouellet@live.ca>
2024-02-07 22:03:23 -0500

    Several dependency not meet

--- serveur/src/main.ts
+++ serveur/src/main.ts
@@ -1,1 +1,19 @@
-console.log("Salut");
+import createServer from "./utils/createServer";
+
+console.log("Bonjour la planète !");
+
+async function main (){
+
+        const app = createServer();
+
+    try {
+        
+         const url = await app.listen(4000,"0.0.0.0");
+    }
+    catch(e){
+         console.error(e);
+         
+    }
+}
+
+main();
