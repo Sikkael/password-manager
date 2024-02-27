a80b82923d920573e8166a86579eb5bc2af12fd0
Sikkael <Mouellet@live.ca>
2024-02-26 22:05:03 -0500

    Updating .gitignore

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import {ChakraProvider} from "@chakra-ui/react";
-import {QueryClient} from "react-query";
+import {QueryClient, QueryClientProvider} from "react-query";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+const queryClient =  new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <ChakraProvider>
-              <Component {...pageProps} />
-          </ChakraProvider>
+  return  <QueryClientProvider client={queryClient}>
+              <ChakraProvider>
+                   <Component {...pageProps} />
+               </ChakraProvider>
+          </QueryClientProvider>
 }
