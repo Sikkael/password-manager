035e07e8768246e471b18773546d72a846e1e143
Sikkael <Mouellet@live.ca>
2024-03-04 21:10:02 -0500

    Update index.txs

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,18 @@
+import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useState } from "react";
+import RegisterForm from "@/components/RegisterForm";
+import LoginForm from "@/components/LoginForm";
+import Vault from "@/components/Vault";
 
-const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+const  Home:NextPage = () => {
+
+  const [step, setStep] = useState<'login' | 'register' | 'vault'>('login');
+
   return (
     <>
       <Head>
@@ -14,7 +21,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+      <main>
+         {step == 'register' && <RegisterForm/>}
+         {step == 'login' && <LoginForm/>}
+         {step == 'vault' && <Vault/>}
+      </main>
     </>
   );
 }
+
+export default Home;
