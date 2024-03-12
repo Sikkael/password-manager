2f2ddd1e5acb6d25787c302c6dda5540d018f433
Sikkael <Mouellet@live.ca>
2024-03-11 21:07:41 -0400

    update user.model.ts and tsconfig file.

--- serveur/src/modules/user/user.model.ts
+++ serveur/src/modules/user/user.model.ts
@@ -1,13 +1,27 @@
 import {  getModelForClass, pre, prop } from "@typegoose/typegoose";
 import argon2 from "argon2";
 
+@pre<User>("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    const hash = await argon2.hash(this.password);
+
+    this.password = hash;
+
+    return next();
+  }
+})
 
 
 export class User {
   @prop({ required: true, unique: true })
-  public email!: string;
+  public email: string;
 
   @prop({ required: true })
-  password?: string;
+  password: string;
 }
 
+export const UserModel = getModelForClass(User, {
+  schemaOptions: {
+    timestamps: true,
+  },
+});
