82d901f4b1a28f0a3823f5eea2787b91e33f5a30
Sikkael <Mouellet@live.ca>
2024-03-11 20:58:05 -0400

    Update user.serice.ts file.

--- serveur/src/modules/user/user.model.ts
+++ serveur/src/modules/user/user.model.ts
@@ -1,9 +1,13 @@
-import { prop } from "@typegoose/typegoose";
+import {  getModelForClass, pre, prop } from "@typegoose/typegoose";
+import argon2 from "argon2";
+
+
 
 export class User {
-    @prop({ required: true, unique: true })
-    email: string;
-  
-    @prop({ required: true })
-    password: string;
-  }
+  @prop({ required: true, unique: true })
+  public email!: string;
+
+  @prop({ required: true })
+  password?: string;
+}
+
