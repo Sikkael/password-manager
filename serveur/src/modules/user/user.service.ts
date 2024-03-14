c345e3752593dd99530bf08e26a3b5d0e8b9d311
Sikkael <Mouellet@live.ca>
2024-03-13 21:03:42 -0400

    Update user.serve.ts

--- serveur/src/modules/user/user.service.ts
+++ serveur/src/modules/user/user.service.ts
@@ -1,5 +1,17 @@
-// Generate salt
+import {UserModel}  from "./user.model";
+import crypto from "crypto";
+// generate salt
+export function generateSalt() {
+  return crypto.randomBytes(64).toString('hex');
+}
+
 
 
 // Create a model
+export async function createUser(input:{
+    hashedPassword:string;
+    email:string;
+}){
+    return UserModel.create(input);
+}
 
