03f335da6c7e3adcedd4b5b31c1b281f12956715
Sikkael <Mouellet@live.ca>
2024-03-23 11:22:00 -0400

    Improving RegisterForm.tsx.

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import { FormControl, FormLabel, Heading } from "@chakra-ui/react";
+import { FormControl, FormErrorMessage, FormLabel, Heading } from "@chakra-ui/react";
 import FormWrapper from "./FormWrapper";
 import { useForm } from "react-hook-form";
 
@@ -12,11 +12,28 @@ function RegisterForm(){
     formState: { errors, isSubmitting },
   } = useForm<{ email: string; password: string; hashedPassword: string }>();
      
-    return <FormWrapper>
+    return <FormWrapper
+            onSubmit={handleSubmit(() => {
+            const password = getValues("password");
+            const email = getValues("email");
+
+       })}
+    >
              <Heading>Register</Heading>
              <FormControl mt="4">
                 <FormLabel htmlFor="email">Email</FormLabel> 
-                <input id="email" placeholder="Email"/>
+                <input id="email" placeholder="Email"
+                  {...register("email", {
+                    required: "Email is required",
+                    minLength: { value: 4, message: "Email must be 4 characters long" },
+                  })}
+                />
+        
+                <FormErrorMessage>
+                  {errors.email && errors.email.message}
+                </FormErrorMessage>
+                
+               
              </FormControl>
            </FormWrapper>;
 
