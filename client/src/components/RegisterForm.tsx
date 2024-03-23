60e90e7d81f1e19cabc2da21cfea32cfd242315c
Sikkael <Mouellet@live.ca>
2024-03-23 11:07:00 -0400

    Update RegisterForm.tsx

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,13 +1,24 @@
-import { Heading } from "@chakra-ui/react";
+import { FormControl, FormLabel, Heading } from "@chakra-ui/react";
 import FormWrapper from "./FormWrapper";
+import { useForm } from "react-hook-form";
 
 function RegisterForm(){
+
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    setValue,
+    formState: { errors, isSubmitting },
+  } = useForm<{ email: string; password: string; hashedPassword: string }>();
      
     return <FormWrapper>
-          <Heading>
-            Register
-          </Heading>
-       </FormWrapper>;
+             <Heading>Register</Heading>
+             <FormControl mt="4">
+                <FormLabel htmlFor="email">Email</FormLabel> 
+                <input id="email" placeholder="Email"/>
+             </FormControl>
+           </FormWrapper>;
 
 }
 
