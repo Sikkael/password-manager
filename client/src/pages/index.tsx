89c41f843b59e2bbcd25c7df6bfc11be2eaffa35
Sikkael <Mouellet@live.ca>
2024-03-24 16:17:43 -0400

    update.index.tsx

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -21,7 +21,7 @@ const  Home:NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className={styles.main}>
          {step == 'register' && <RegisterForm/>}
          {step == 'login' && <LoginForm/>}
          {step == 'vault' && <Vault/>}
