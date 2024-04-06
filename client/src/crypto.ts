ef4bc7f16e6173c73245650f6202c14ed8cfe6e8
Sikkael <Mouellet@live.ca>
2024-04-06 15:23:30 -0400

    Update crypto.ts

--- client/src/crypto.ts
+++ client/src/crypto.ts
@@ -1,7 +1,7 @@
 
 
 import pbkdf2 from "crypto-js/pbkdf2";
-import { SHA256 } from "crypto-js";
+import { AES, enc ,SHA256 } from "crypto-js";
 
 export function hashPassword(password:string){
      return SHA256(password).toString();
@@ -22,3 +22,30 @@ export function generateVaultKey({
        keySize: 32,
      }).toString();
    }
+
+   export function decryptVault({
+    vaultKey,
+    vault,
+  }: {
+    vaultKey: string;
+    vault: string;
+  }) {
+    const bytes = AES.decrypt(vault, vaultKey);
+    const decrypted = bytes.toString(enc.Utf8);
+  
+    try {
+      return JSON.parse(decrypted).vault;
+    } catch (e) {
+      return null;
+    }
+  }
+  
+  export function encryptVault({
+    vaultKey,
+    vault,
+  }: {
+    vaultKey: string;
+    vault: string;
+  }) {
+    return AES.encrypt(vault, vaultKey).toString();
+  }
