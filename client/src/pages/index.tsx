c2cad9ba1d68821eca38cac7821c76a1473cd61b
Sikkael <Mouellet@live.ca>
2024-04-22 15:03:58 -0400

    Update index.tsx

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -46,12 +46,9 @@ const  Home:NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
       <main className={styles.main}>
-        
-            
-          
-        
-        
+       
       {step === "register" && (
           <RegisterForm setStep={setStep} setVaultKey={setVaultKey} />
         )}
