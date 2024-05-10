99f117cc8b558b7bee640e300bc3a8bec41b9f2e
Sikkael <Mouellet@live.ca>
2024-05-09 21:38:51 -0400

    Revert "Update RegisterForm.tsx"
    This reverts commit be9e874981ff02c02bc6bd817e25bfad45b34869.

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -30,11 +30,11 @@ function RegisterForm({
     register,
     getValues,
     setValue,
-    formState: { errors, isSubmitting,isValid },
+    formState: { errors, isSubmitting },
   } = useForm<{ email: string; password: string; hashedPassword: string }>(
     
   );
-    
+    const [input, setInput] = useState('')
     
     
     const mutation = useMutation(registerUser, {
@@ -81,7 +81,7 @@ function RegisterForm({
       }
     >
              <Heading>Register</Heading>
-             <FormControl mt="4"  isInvalid={errors.email} >
+             <FormControl mt="4"  _invalid={errors.email} >
                 <FormLabel htmlFor="email">Email</FormLabel> 
                 <Input id="email" placeholder="Email"  
                   {...register("email", {
